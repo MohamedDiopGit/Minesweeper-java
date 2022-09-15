cfa5ed46478ff9082f79ac905590345b2d2b4f4f
MohamedDiopGit <diopm.contact@gmail.com>
2022-09-15 09:47:14 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Random;
+
 public class Main {
+    private static final int MAX = 10;
+
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        Random gene = new Random() ;
+        int nbAlea = gene.nextInt(MAX);
+        System.out.println("nbAlea: " + nbAlea);
     }
 }
